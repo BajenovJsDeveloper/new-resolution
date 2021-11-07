094160b24103c8b592eb8982e3ba20291dadf111
Vladimir <vladimir.bajenov@bigdatafed.com>
2021-11-08 00:00:06 +0200

    Updates

---
+++ index.10e80d01.js
@@ -0,0 +1,2 @@
+function getResolution(){let e=window.innerWidth,n=window.innerHeight;document.querySelector(".text-resolution").innerHTML=`${e} x ${n}`}window.onresize=getResolution,getResolution();
+//# sourceMappingURL=index.10e80d01.js.map
